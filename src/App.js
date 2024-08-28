db4fba92ac445f3fccc0689620c9b29a0bce3402
jovohir-zoyirov <javohir.zoyirov.official@gmail.com>
2024-08-28 10:07:59 +0500

    Yangi kod qo‘shildi

--- src/App.js
+++ src/App.js
@@ -6,26 +6,30 @@ import phone from "./image/Phone call.png";
 import tg from "./image/Telegram.png";
 import video from "./image/v1.mp4";
 import { useState } from "react";
-import tom1 from "./image/tom1.png"
-import tom2 from "./image/tom2.png"
-import tom3 from "./image/tom3.png"
-import tom4 from "./image/tom4.png"
-import tom5 from "./image/tom5.png"
-import tom6 from "./image/tom6.png"
-import tom7 from "./image/tom7.png"
-import tom8 from "./image/tom8.png"
-import tom9 from "./image/tom9.png"
-import tom10 from "./image/tom10.png"
+import tom1 from "./image/tom1.png";
+import tom2 from "./image/tom2.png";
+import tom3 from "./image/tom3.png";
+import tom4 from "./image/tom4.png";
+import tom5 from "./image/tom5.png";
+import tom6 from "./image/tom6.png";
+import tom7 from "./image/tom7.png";
+import tom8 from "./image/tom8.png";
+import tom9 from "./image/tom9.png";
+import tom10 from "./image/tom10.png";
 import { Modal } from "antd";
 import { useTranslation } from "react-i18next";
-import "./i18n"
+import "./i18n";
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [language, setLanguage] = useState("uz");
-  const {t, i18n} = useTranslation();
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [location, setLocation] = useState("");
+  const [order, setOrder] = useState([]);
+  const { t, i18n } = useTranslation();
   const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng)
-  }
+    i18n.changeLanguage(lng);
+  };
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -35,10 +39,25 @@ function App() {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+  const orderBtn = () => {
+    setOrder((prev) => [
+      ...prev,
+      {
+        name: name,
+        phone: phoneNumber,
+        location: location,
+      },
+    ]);
+  };
+  console.log(order,"order");
   return (
-    <div className="container-fluid fluid h-100  d-flex flex-column justify-content-between">
+    <div style={{height:'100vh'}} className="container-fluid fluid d-flex mx-auto flex-column justify-content-between">
       <div className="d-flex align-items-center justify-content-between mt-4">
-        <img style={{ width: "210px", height: "39px", objectFit:"cover" }} src={logo} alt="logo" />
+        <img
+          style={{ width: "210px", height: "39px", objectFit: "cover" }}
+          src={logo}
+          alt="logo"
+        />
         <div className="nav-item p-0 m-0 dropdown1 position-relative d-inline-block">
           <button className="btn border-0 dropbtn d-flex align-items-center">
             <img
@@ -65,7 +84,10 @@ function App() {
                   language === "uz" ? "d-none dropdown-item" : "dropdown-item"
                 }
                 type="button"
-                onClick={() => {setLanguage("uz");changeLanguage("uz")}}
+                onClick={() => {
+                  setLanguage("uz");
+                  changeLanguage("uz");
+                }}
               >
                 <img
                   style={{
@@ -80,7 +102,10 @@ function App() {
             </div>
             <div className="">
               <button
-                onClick={() => {setLanguage("en");changeLanguage("en")}}
+                onClick={() => {
+                  setLanguage("en");
+                  changeLanguage("en");
+                }}
                 className={
                   language === "en" ? "d-none dropdown-item" : "dropdown-item"
                 }
@@ -99,7 +124,10 @@ function App() {
             </div>
             <div className="">
               <button
-                onClick={() => {setLanguage("ru");changeLanguage("ru")}}
+                onClick={() => {
+                  setLanguage("ru");
+                  changeLanguage("ru");
+                }}
                 className={
                   language === "ru" ? "d-none dropdown-item" : "dropdown-item"
                 }
@@ -122,29 +150,29 @@ function App() {
       <div className="row">
         <div className="col-12 col-lg-6 col-md-6 div1 d-flex justify-content-center align-items-center">
           <div>
-          <p style={{ color: "#0763C0" }} className="fw-bold mt-4 text1">
-            {t('text1')}
-          </p>
-          <p className="fw-bold my-4 text2" style={{ color: "#0763C0" }}>
-            {t('text2')}
-          </p>
-          <button
-            style={{ background: "#0763C0" }}
-            className="btn fw-bold text-white button"
-            onClick={showModal}
-          >
-            {t('button')}
-          </button>
-          <p style={{ color: "#0763C0" }} className="fw-bold mt-4 ltr">
-            18,9 {t('l')}
-          </p>
+            <p style={{ color: "#0763C0" }} className="fw-bold mt-4 text1">
+              {t("text1")}
+            </p>
+            <p className="fw-bold my-4 text2" style={{ color: "#0763C0" }}>
+              {t("text2")}
+            </p>
+            <button
+              style={{ background: "#0763C0" }}
+              className="btn fw-bold text-white button"
+              onClick={showModal}
+            >
+              {t("button")}
+            </button>
+            <p style={{ color: "#0763C0" }} className="fw-bold mt-4 ltr">
+              18,9 {t("l")}
+            </p>
           </div>
         </div>
         <div className="col-12 col-lg-6 col-md-6 div2 d-flex align-items-center justify-content-center">
-          <video 
+          <video
             className="rotated-video"
             style={{ width: "100%" }}
-            autoPlay  
+            autoPlay
             muted
             loop
           >
@@ -152,7 +180,7 @@ function App() {
           </video>
         </div>
       </div>
-      <div className="d-flex align-items-center flex-wrap gap-3 tgPhone">
+      <div className="d-flex align-items-center flex-wrap gap-3 mb-3 tgPhone">
         <a
           href="tel:+99833 444-55-50"
           style={{ cursor: "pointer", textDecoration: "none" }}
@@ -183,64 +211,72 @@ function App() {
         </a>
       </div>
       <Modal
-            open={isModalOpen}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            footer={null}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+        width={350}
+      >
+        <div className="text-center d-flex gap-4 align-items-center flex-column h-100">
+          <p
+            style={{ color: "#0763C0", fontSize: "40px" }}
+            className="fw-bold w-100 p-0 m-0 zayavka"
           >
-            <div className="text-center d-flex gap-4 align-items-center flex-column px-4 h-100">
-              <p
-                style={{ color: "#0763C0", fontSize: "40px" }}
-                className="fw-bold w-100 p-0 m-0 zayavka"
-              >
-                {t('modaltext1')}
-              </p>
-              <p
-                style={{ color: "#333334", fontSize: "20px" }}
-                className="w-100 p-0 m-0 inputtext"
-              >
-                {t('modaltext2')}
-              </p>
-              <input
-                style={{
-                  background: "#D4E9FA",
-                  fontSize: "15px",
-                  color: "#333334",
-                }}
-                placeholder={t('input1')}
-                className="form-control rounded-5 border-0 inputtext"
-              />
-              <input
-                style={{
-                  background: "#D4E9FA",
-                  fontSize: "15px",
-                  color: "#333334",
-                }}
-                placeholder={t('input2')}
-                className="form-control rounded-5 border-0 inputtext"
-              />
-              <input
-                style={{
-                  background: "#D4E9FA",
-                  fontSize: "15px",
-                  color: "#333334",
-                }}
-                placeholder={t('input3')}
-                className="form-control rounded-5 border-0 inputtext"
-              />
-              <button
-                style={{
-                  background: "#0763C0",
-                  color: "white",
-                  fontSize: "18px",
-                }}
-                className="fw-bold rounded-5 border-0 px-4 py-2 "
-                onClick={handleOk}
-              >
-                {t('btn')}
-              </button>
-            </div>
-          </Modal>
+            {t("modaltext1")}
+          </p>
+          <p
+            style={{ color: "#333334", fontSize: "20px" }}
+            className="w-100 p-0 m-0 inputtext"
+          >
+            {t("modaltext2")}
+          </p>
+          <input
+            style={{
+              background: "#D4E9FA",
+              fontSize: "15px",
+              color: "#333334",
+            }}
+            onChange={(e) => setName(e.target.value)}
+            placeholder={t("input1")}
+            className="form-control rounded-5 border-0 inputtext"
+          />
+          <input
+            style={{
+              background: "#D4E9FA",
+              fontSize: "15px",
+              color: "#333334",
+            }}
+            type="number"
+            disabled = {name === ""}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder={t("input2")}
+            className="form-control rounded-5 border-0 inputtext"
+          />
+          <input
+            style={{
+              background: "#D4E9FA",
+              fontSize: "15px",
+              color: "#333334",
+            }}
+            disabled = {phoneNumber === ""}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder={t("input3")}
+            className="form-control rounded-5 border-0 inputtext"
+          />
+          <button
+            style={{
+              background: "#0763C0",
+              color: "white",
+              fontSize: "18px",
+            }}
+            disabled={name ==="" || phoneNumber==="" || location===""}
+            className="fw-bold rounded-5 border-0 px-4 py-2 "
+            onClick={()=> {orderBtn();handleOk()}}
+          >
+            {t("btn")}
+          </button>
+        </div>
+      </Modal>
       <img className="tom1" src={tom1} alt="#" />
       <img className="tom2" src={tom2} alt="#" />
       <img className="tom3" src={tom3} alt="#" />
